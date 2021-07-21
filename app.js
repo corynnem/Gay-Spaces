c9b9104961f527ebd379476cf9671290f2fe39d2
Corynne Moody <cmoody@elevenfifty.org>
2021-07-21 11:21:12 -0400

    pushing base files; added dbconnection and app listening on 8080

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const Express = require('express')
+const db = require('./db')
+const app = Express();
+
+
+app.use(Express.json())
+app.use(cors)
+
+//base endpoint
+app.get('/test', (req, res) => {
+    res.json({
+        message: "welcome to t4t"
+    })
+})
+
+db.authenticate()
+.then(() => db.sync())
+// .then(() => db.sync({force: true}))
+.then(() => app.listen( process.env.PORT, () => {
+    console.log(`[t4t-server]: app listening on ${process.env.PORT}`)
+}))
+
